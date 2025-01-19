96b0cba5a0a334639883aa34ff0ee28dd6832c0e
Joaquincg200 <joaquincaravacagarcia200@gmail.com>
2025-01-19 13:44:53 +0100

    noveno commit

--- src/js/main.js
+++ src/js/main.js
@@ -68,3 +68,32 @@ document.querySelectorAll("a").forEach((link) => {
   });
 });
 
+//Slider
+const sliderItems = document.querySelectorAll(".simple-slider .slider-item");
+let currentIndex = 0;
+
+function imagenMostrar(index) {
+  sliderItems.forEach((item) => item.classList.remove("active"));
+
+  if (index >= sliderItems.length) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = sliderItems.length - 1;
+  }
+
+  sliderItems[currentIndex].classList.add("active");
+}
+
+imagenMostrar(currentIndex);
+
+const prevBtn = document.getElementById("prevBtn");
+prevBtn.addEventListener("click", () => {
+  currentIndex--;
+  imagenMostrar(currentIndex);
+});
+
+const nextBtn = document.getElementById("nextBtn");
+nextBtn.addEventListener("click", () => {
+  currentIndex++;
+  imagenMostrar(currentIndex);
+});
